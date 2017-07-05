2a10dc44390f261f8d990e4f7c590df9a3974925
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2017-07-05 11:33:00 +0200

    Start test nodes in order

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sbt run -Dakka.remote.netty.tcp.port=2552 -Dbenchmark.expected-members=3 &
+sleep 10
 sbt run -Dakka.remote.netty.tcp.port=2553 -Dbenchmark.expected-members=3 &
+sleep 10
 sbt run -Dakka.remote.netty.tcp.port=2554 -Dbenchmark.expected-members=3 &
 
 
