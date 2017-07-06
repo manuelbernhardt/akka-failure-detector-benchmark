9c9e6e51eee6e400bf9fb5f99b9391605d6b19d5
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2017-07-06 10:42:44 +0200

    Stricted implementation of the paper, we'll see if we need optimization later on

--- src/main/scala/akka/remote/NewAdaptiveAccrualFailureDetector.scala
+++ src/main/scala/akka/remote/NewAdaptiveAccrualFailureDetector.scala
@@ -1,15 +1,11 @@
 package akka.remote
 
-import akka.event.Logging.Warning
 import akka.remote.FailureDetector.Clock
 import java.util.concurrent.atomic.AtomicReference
 
 import scala.annotation.tailrec
-import scala.concurrent.duration.Duration
-import scala.concurrent.duration.FiniteDuration
 import com.typesafe.config.Config
 import akka.event.EventStream
-import akka.util.Helpers.ConfigOps
 
 /**
  * Implementation of 'A New Adaptive Accrual Failure Detector for Dependable Distributed Systems' by Satzget al. as defined in their paper:
@@ -35,13 +31,8 @@ import akka.util.Helpers.ConfigOps
  *   actual crashes
  * @param maxSampleSize Number of samples to use for calculation of mean and standard deviation of
  *   inter-arrival times.
- * @param acceptableHeartbeatPause Duration corresponding to number of potentially lost/delayed
- *   heartbeats that will be accepted before considering it to be an anomaly.
- *   This margin is important to be able to survive sudden, occasional, pauses in heartbeat
- *   arrivals, due to for example garbage collect or network drop.
- * @param firstHeartbeatEstimate Bootstrap the stats with heartbeats that corresponds to
- *   to this duration, with a with rather high standard deviation (since environment is unknown
- *   in the beginning)
+ * @param scalingFactor A scaling factor to prevent the failure detector to overestimate the probability of failures
+ *   particularly in the case of increasing network latency times
  * @param clock The clock, returning current time in milliseconds, but can be faked for testing
  *   purposes. It is only used for measuring intervals (duration).
  */
@@ -49,8 +40,6 @@ class NewAdaptiveAccrualFailureDetector(
   val threshold:                Double,
   val maxSampleSize:            Int,
   val scalingFactor:            Double,
-  val acceptableHeartbeatPause: FiniteDuration,
-  val firstHeartbeatEstimate:   FiniteDuration,
   eventStream:                  Option[EventStream])(
   implicit
   clock: Clock) extends FailureDetector {
@@ -66,26 +55,13 @@ class NewAdaptiveAccrualFailureDetector(
       threshold = config.getDouble("threshold"),
       maxSampleSize = config.getInt("max-sample-size"),
       scalingFactor = config.getDouble("scaling-factor"),
-      acceptableHeartbeatPause = config.getMillisDuration("acceptable-heartbeat-pause"),
-      firstHeartbeatEstimate = config.getMillisDuration("heartbeat-interval"),
       Some(ev))
 
   require(threshold > 0.0, "failure-detector.threshold must be > 0")
   require(maxSampleSize > 0, "failure-detector.max-sample-size must be > 0")
-  require(scalingFactor > 1, "failure-detector.scaling-factor must be > 1")
-  require(acceptableHeartbeatPause >= Duration.Zero, "failure-detector.acceptable-heartbeat-pause must be >= 0")
-  require(firstHeartbeatEstimate > Duration.Zero, "failure-detector.heartbeat-interval must be > 0")
+  require(scalingFactor > 0, "failure-detector.scaling-factor must be > 0")
 
-  // guess statistics for first heartbeat,
-  // important so that connections with only one heartbeat becomes unavailable
-  private val firstHeartbeat: HeartbeatHistory = {
-    // bootstrap with 2 entries with rather high standard deviation
-    val mean = firstHeartbeatEstimate.toMillis
-    val stdDeviation = mean / 4
-    HeartbeatHistory(maxSampleSize) :+ (mean - stdDeviation) :+ (mean + stdDeviation)
-  }
-
-  private val acceptableHeartbeatPauseMillis = acceptableHeartbeatPause.toMillis
+  private val firstHeartbeat: HeartbeatHistory = HeartbeatHistory(maxSampleSize)
 
   /**
    * Implement using optimistic lockless concurrency, all state is represented
@@ -110,17 +86,12 @@ class NewAdaptiveAccrualFailureDetector(
     val newHistory = oldState.freshnessPoint match {
       case None ⇒
         // this is heartbeat from a new resource
-        // add starter records for this new resource
+        // according to the algorithm do not add any initial history
         firstHeartbeat
-      case Some(latestTimestamp) ⇒
+      case Some(freshnessPoint) ⇒
         // this is a known connection
-        val interval = timestamp - latestTimestamp
-        // don't use the first heartbeat after failure for the history, since a long pause will skew the stats
-        if (isAvailable(timestamp)) {
-          if (interval >= (acceptableHeartbeatPauseMillis / 2) && eventStream.isDefined)
-            eventStream.get.publish(Warning(this.toString, getClass, s"heartbeat interval is growing too large: $interval millis"))
-          oldState.history :+ interval
-        } else oldState.history
+        val tΔ = timestamp - freshnessPoint
+        oldState.history :+ tΔ
     }
 
     val newState = oldState.copy(history = newHistory, freshnessPoint = Some(timestamp)) // record new timestamp
@@ -130,10 +101,9 @@ class NewAdaptiveAccrualFailureDetector(
   }
 
   /**
-   * The suspicion level of the accrual failure detector (expressed as percentage).
+   * The suspicion level of the accrual failure detector (expressed as probability).
    *
-   * If a connection does not have any records in failure detector then it is
-   * considered healthy.
+   * If a connection does not have any records in failure detector then it is considered healthy.
    */
   def suspicion: Double = suspicion(clock())
 
@@ -141,16 +111,14 @@ class NewAdaptiveAccrualFailureDetector(
     val oldState = state.get
     val freshnessPoint = oldState.freshnessPoint
 
-    if (freshnessPoint.isEmpty) 0.0 // treat unmanaged connections, e.g. with zero heartbeats, as healthy connections
+    if (freshnessPoint.isEmpty || oldState.history.intervals.isEmpty) 0.0 // treat unmanaged connections, e.g. with zero heartbeats, as healthy connections
     else {
-      val timeDiff = timestamp - freshnessPoint.get
+      val tΔ = timestamp - freshnessPoint.get
+      val S = oldState.history.intervals
+      val SLength = S.length
+      val StΔLength = S.count(_ <=  tΔ * scalingFactor)
 
-      val history = oldState.history
-
-      val intervalLength = history.intervals.length
-      val intervalTimeDeltaLength = history.intervals.count(_ <=  timeDiff * scalingFactor)
-
-      intervalTimeDeltaLength.toDouble / intervalLength.toDouble
+      StΔLength.toDouble / SLength.toDouble
     }
   }
 
